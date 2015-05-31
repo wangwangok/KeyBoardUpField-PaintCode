a36341767381e5c38c5c0d2d4cd49a9720ee2ffa
王望 <wangwang@enveesoft.com>
2015-05-31 18:29:45 +0800

    Initial Commit

---
+++ KeyBoardUpField/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KeyBoardUpField
+//
+//  Created by 王望 on 15/5/31.
+//  Copyright (c) 2015年 王望. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
